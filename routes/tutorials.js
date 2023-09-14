1a287130aad2ca76e6afb1281a5c4ddbe1d5187f
Noroff BED <124564549+noroff-bed1@users.noreply.github.com>
2023-09-14 14:09:33 +0200

    Add files via upload

---
+++ routes/tutorials.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var TutorialService = require('../services/TutorialService');
+const { Op } = require('sequelize');
+var db = require('../models');
+var tutorialService = new TutorialService(db);
+
+/* GET tutorials listing. */
+router.get('/', async function (req, res, next) {
+	const { sort, title, description, published, page, size } = req.query;
+	const order = sort ? sort.split(',').map((pair) => pair.split(':')) : [];
+	const titleCondition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+	const descCondition = description ? { description: { [Op.like]: `%${description}%` } } : null;
+	const publishedCondition = published ? { published: { [Op.like]: `%${published}%` } } : null;
+	const condition = { [Op.and]: [titleCondition, descCondition, publishedCondition] };
+	const pagination = getPagination(page, size);
+	const tutorials = await tutorialService.getAll(condition, order, pagination);
+	res.render('tutorials', { tutorials: tutorials });
+});
+
+const getPagination = (page, size) => {
+	const limit = size ? +size : 5;
+	const offset = page ? (page - 1) * limit : 0;
+	return { limit, offset };
+};
+
+module.exports = router;
+
---
+++ services/TutorialService.js
@@ -0,0 +1,22 @@
+class TutorialService {
+	constructor(db) {
+		this.client = db.sequelize;
+		this.tutorial = db.tutorial;
+	}
+
+	async getAll(condition, order, pagination) {
+		return this.tutorial
+			.findAll({
+				limit: pagination.limit,
+				offset: pagination.offset,
+				where: condition,
+				order: order,
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	}
+}
+module.exports = TutorialService;
+// Look at findAndCountAll for pagination
+
